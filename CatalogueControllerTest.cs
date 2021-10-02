5604315b298fc280d698dcb39957530fc7558610
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2021-10-02 15:12:30 +0100

    développement test unitaire de la partie ajouter une catégorie

--- CatalogueControllerTest.cs
+++ CatalogueControllerTest.cs
@@ -9,6 +9,9 @@ using Catalogue_Produit_App;
 using Catalogue_Produit_App.Controllers;
 using Catalogue_Produit_App.Service;
 using Moq;
+using Catalogue_Produit_App.Models;
+using Catalogue_Produit_App.Helper;
+using Catalogue_Produit_App.DTO;
 
 namespace Catalogue_Produit_Test_Unitaires
 
@@ -16,20 +19,56 @@ namespace Catalogue_Produit_Test_Unitaires
     [TestClass]
     public class CatalogueControllerTest
     {
-        private Mock<ICategorieService> _categorieService = new Mock<ICategorieService>();
+        private readonly Mock<ICategorieService> _categorieService;
+        private readonly CategorieHelper _categorieHelper = new CategorieHelper();
+        private readonly CatalogueController _controller;
+        public CatalogueControllerTest()
+        {
+            _categorieService = new Mock<ICategorieService>();
+            _controller = new CatalogueController(_categorieService.Object);
+        }
         [TestMethod]
-        public void TestAjoutCatalogueMethod()
+        public void TestGetAllCatalogueCatalogueMethod()
         {
             //Arrange
 
-            CatalogueController controller = new CatalogueController(_categorieService.Object);
 
             //Act
-            ViewResult result = controller.AjoutCatalogue() as ViewResult;
+            ViewResult result = _controller.AjoutCatalogue() as ViewResult;
             string viewName = result.ViewName;
 
             //Assert
             Assert.AreEqual(viewName,"");
+        }
+
+
+
+        [TestMethod]
+        public void TestAjouterCatalogueCatalogueMethod()
+        {
+            //Arrange
+            CAT_CATEGORIE categorieTransfert = null;
+            CategorieDto categorie = null;
+            _categorieService.Setup(r => r.AddNewCategorie(It.IsAny<CategorieDto>()))
+                .Callback<CategorieDto>(x => categorie = x);
+
+            //Act
+            var categorieDto = new CategorieDto
+            {
+                libelleCategorie = "Test categorie",
+                codeCategorie = 0,
+                dateSaisie = DateTime.Now
+            };
+            categorieTransfert = _categorieHelper.ConvertFromDto(categorieDto);
+            _controller.AjoutCatalogue(categorieTransfert);
+            _categorieService.Verify(x => x.AddNewCategorie(It.IsAny<CategorieDto>()), Times.Once);
+           
+
+
+            //Assert
+            Assert.AreEqual(categorie.libelleCategorie, categorieDto.libelleCategorie);
+            Assert.AreEqual(categorie.codeCategorie, categorieDto.codeCategorie);
+            //Assert.AreEqual(categorie.dateSaisie, categorieDto.dateSaisie);
         }
     }
 }
